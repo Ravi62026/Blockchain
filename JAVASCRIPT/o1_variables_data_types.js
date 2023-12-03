396ff57d97b8ef5f9a94be5d0af14579267ac557
Ravi Shankar <128376014+Ravi62026@users.noreply.github.com>
2023-12-03 13:57:45 +0530

    Create o1_variables_data_types.js

---
+++ JAVASCRIPT/o1_variables_data_types.js
@@ -0,0 +1,72 @@
+"use strict"
+
+// let num = 7;
+// console.log(num);
+// num = num + 3;
+// console.log(num);
+
+// Dynamically typed language nature of java cript
+
+// let x = 6;
+// console.log(x);
+// x = "Ravi";
+// console.log(x);
+// x = 8.9;
+// console.log(x);
+
+// typeof operator
+
+// let value = "ravi";
+// console.log(typeof value);  // string
+
+// let y;
+// console.log(typeof y);  // undefined
+
+// type conversion
+
+// let value = "789";
+// console.log(typeof value);  // string
+// value = Number(value);
+// console.log(typeof value);  // number
+
+// NAN - not a number
+
+// let value = Number("987 Ravi");
+// console.log(typeof value);
+
+// let value = parseInt("987 Ravi");
+// console.log(value , typeof value);  // 987 number
+
+
+
+
+// Data types
+
+// primitive
+// number , string , boolean , null , undefined , symbol
+
+// reference
+// object , array , function
+
+// object introduction
+
+// let obj = {
+//     name : "Ravi",
+//     reg : "22MEI10050",
+//     age : 19
+// }
+
+// let age = obj.age;
+// console.log(age);
+// console.log(typeof age);
+
+// obj.age = 20;
+// console.log(obj.age);
+// console.log(typeof obj);
+// console.log(obj);
+
+// let fname = "ravi";
+// let lname = "shankar";
+// console.log(fname + " " + lname);
+// let name = fname + lname;
+// console.log(name);
